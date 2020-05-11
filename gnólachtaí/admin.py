b4ea6ff66034ab03d6aed2954f59b64460de1a24
Seanán Ó Coistín <seananoc@gmail.com>
2020-05-11 19:32:11 +0000

    Added new home page, new search bar, add a listing button on menu

---
+++ gnólachtaí/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Gnólacht, Aicme
+
+admin.site.register(Gnólacht)
+admin.site.register(Aicme)
+
+
---
+++ gnólachtaí/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GnólachtaíConfig(AppConfig):
+    name = 'gnólachtaí'
