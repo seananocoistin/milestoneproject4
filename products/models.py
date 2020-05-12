f5f9634a9e6b0714f4c4701cad0bbb189a39efd2
Seanán Ó Coistín <seananoc@gmail.com>
2020-05-12 18:03:04 +0000

    Added products and categories

--- products/models.py
+++ products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Category(models.Model):
-
+  
     class Meta:
         verbose_name_plural = 'Categories'
 
@@ -11,7 +11,7 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
-    
+
     def get_friendly_name(self):
         return self.friendly_name
 
