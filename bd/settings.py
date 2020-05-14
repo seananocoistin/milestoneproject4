31bef0bb8926552148edd6ff39547dc647a5630e
Seanán Ó Coistín <seananoc@gmail.com>
2020-05-14 13:19:09 +0000

    Added sorting features and I deleted unneeded directories

--- bd/settings.py
+++ bd/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'home',
-    'gnólachtaí',
     'products',
 ]
 
--- gnólachtaí/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GnólachtaíConfig(AppConfig):
-    name = 'gnólachtaí'
