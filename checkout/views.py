ed59312693d72d8175c06881ec1e947c7ccb1b88
Seanán Ó Coistín <seananoc@gmail.com>
2020-05-19 11:35:05 +0000

    Added Stripe elements in JS and CSS

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,7 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_4KgVlDgFauT9Sfs8TtAN5Tjd00qMznyVjp',
     }
 
     return render(request, template, context)
