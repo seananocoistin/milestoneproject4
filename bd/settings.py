7ba8cd1caceb0bced546a7a54702c4e83862e119
Seanán Ó Coistín <seananoc@gmail.com>
2020-05-19 21:24:55 +0000

    Started creating the profiles app

--- bd/settings.py
+++ bd/settings.py
@@ -47,7 +47,8 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
-
+    'profiles',
+    
     # Other
     'crispy_forms',
 ]
--- checkout/models.py
+++ checkout/models.py
@@ -7,6 +7,7 @@ from django.conf import settings
 from django_countries.fields import CountryField
 
 from products.models import Product
+from profiles.models import UserProfile
 
 
 class Order(models.Model):
@@ -14,12 +15,12 @@ class Order(models.Model):
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
-    country = CountryField(blank_label='Country *', null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     county = models.CharField(max_length=80, null=True, blank=True)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
     date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
