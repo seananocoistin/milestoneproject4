e77c69dfb9afd2c22bd8a59a6465918a3f99afa5
Seanán Ó Coistín <seananoc@gmail.com>
2020-05-20 11:39:59 +0000

    Added profile management features

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def profile(request):
+    template = 'profiles/profile.html'
+    context = {}
+
+    return render(request, template, context)
